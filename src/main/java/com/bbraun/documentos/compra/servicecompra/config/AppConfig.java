f23dc5e7758a63eb5d7fbe9a2b2c9244a3ef5fff
melani yulli laveriano porroa <melani.laveriano@unmsm.edu.pe>
2024-07-19 12:36:24 -0500

    fin

--- src/main/java/com/bbraun/documentos/compra/servicecompra/config/AppConfig.java
+++ src/main/java/com/bbraun/documentos/compra/servicecompra/config/AppConfig.java
@@ -2,13 +2,29 @@ package com.bbraun.documentos.compra.servicecompra.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate(){
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add((request, body, execution) -> {
+            String token = ((JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication()).getToken().getTokenValue();
+            request.getHeaders().set("Authorization", "Bearer " + token);
+            return execution.execute(request, body);
+        });
+
+        restTemplate.setInterceptors(interceptors);
+        return restTemplate;
     }
 }
